78c97aa370c1bcd1446578c39647d4ca361ddbc5
Badri-Prasad-Verma <jmnaprasad261@gmail.com>
2024-09-17 00:16:57 +0530

    added csv and excel format

--- emailService/src/main/java/com/rabbit/service/EmailService.java
+++ emailService/src/main/java/com/rabbit/service/EmailService.java
@@ -1,9 +1,14 @@
 package com.rabbit.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.core.io.InputStreamSource;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.core.io.ByteArrayResource;
+import java.io.InputStream;
 
 @Service
 public class EmailService {
@@ -11,12 +16,25 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
-    public void sendEmail(String to, String subject, String body) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(body);
-        mailSender.send(message);
+    public void sendEmailWithAttachment(String to, String subject, String text, String fileName, InputStream fileContent) {
+        MimeMessage message = mailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(text);
+
+            InputStreamSource attachment = new ByteArrayResource(fileContent.readAllBytes());
+            helper.addAttachment(fileName, attachment);
+
+            mailSender.send(message);
+            System.out.println("Email sent successfully to " + to);
+        } catch (MessagingException | java.io.IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
